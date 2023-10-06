c76d0d4c6d9249044aa531559e2d7315e25055ec
Madihakhan1 <144325902+Madihakhan1@users.noreply.github.com>
2023-10-06 09:08:48 +0200

    Add

--- Task2/src/Cafe.java
+++ Task2/src/Cafe.java
@@ -9,19 +9,26 @@ public class Cafe {
     private final ArrayList<String> coffeeMenu = new ArrayList<>();
 
     public void loadMenuData() {
+        // Opret en filreference til stien til din kaffemenu-fil
         File file = new File("/Users/madihakhan/IdeaProjects/Task2/src/coffees.txt");
 
         try {
+            // Opret en scanner til at læse filen
             Scanner scan = new Scanner(file);
 
             while (scan.hasNextLine()) {
+                // Læs næste linje fra filen
                 String result = scan.nextLine();
+
+                // Tilføj den læste linje til coffeeMenu (antages at coffeeMenu er en liste)
                 coffeeMenu.add(result);
             }
         } catch (Exception e) {
+            // Hvis der opstår en fejl, fanger vi den her, men det er vigtigt at behandle fejlen (f.eks. ved at udskrive den)
             e.getMessage();
         }
     }
+
 
     public void printMenu() {
         for (String menu : coffeeMenu) {
